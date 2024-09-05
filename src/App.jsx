b1617abb5cd7bd8ed73a09d35886c7cd21334da5
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-09-05 21:19:59 +0900

    feat: create TabButton component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import CoreConcept from './components/CoreConcept.jsx';
 import Header from './components/Header/Header.jsx';
+import TabButton from './components/TabButton.jsx';
 import { CORE_CONCEPTS } from './data.js';
 
 function App() {
@@ -15,6 +16,12 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>안녕하십니다람쥐</TabButton>
+          </menu>
         </section>
       </main>
     </div>
